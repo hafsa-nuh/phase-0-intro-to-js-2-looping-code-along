a0a1ac1a850285efefec534d2b54362c9be86288
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-04 17:59:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solutions in this file
+
+// let cards = [`Guadalupe`,`Ollie`,`Aki`];
+
+// function writeCards(cards , event) {
+//     for (let i = 0; i < cards.length; i++) {
+//         console.log(`Thank you, ${cards[i]} for the wonderful ${event} gift!`);
+//       return cards;}
+// }
+
+
+// function writeCards(cards, event) {
+//     return  cards.map(cards=> `Thank you, ${cards} for the wonderful ${event} gift!`)
+//  }
+ 
+//  console.log(writeCards(cards, "birthday"))
+
+// function writeCards(names, event) {
+//     let messages = [`Guadalupe`,`Ollie`,`Aki`]
+//     for (let i = 0; i < names.length; i++) {
+//       messages.push(`Thank you, ${names[i]} for the wonderful ${event} gift!`)
+//     }
+//     return messages;
+// }
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown(n){
+    while (n>0){
+        console.log(n);
+        n--;
+    } 
+    console.log(n)
+}
+
+// console.log(writeCards(["Guadalupe","Ollie","Aki"], 'wedding'));
+
+// countDown(10);
+// let countDown = 0;
+// while (countDown <= 10) {
+//   console.log(countDown);
+//   countDown++;
+// }
+
+
+
+
+
